e5f8e046deb8cb389d5b9da252285bb23390a336
Jesse Nelson <spheromak@gmail.com>
2017-06-03 02:02:51 -0600

    Got keyboard input working

---
+++ keyboard.go
@@ -0,0 +1,65 @@
+package main
+
+import "github.com/nsf/termbox-go"
+
+type keyboardEventType int
+type direction int
+
+const (
+	MOVE keyboardEventType = iota
+	RETRY
+	END
+
+	RIGHT direction = 1 + iota
+	LEFT
+	UP
+	DOWN
+)
+
+type keyboardEvent struct {
+	eventType keyboardEventType
+	key       termbox.Key
+}
+
+func keyToDirection(k termbox.Key) direction {
+	switch k {
+	case termbox.KeyArrowLeft:
+		return LEFT
+	case termbox.KeyArrowDown:
+		return DOWN
+	case termbox.KeyArrowRight:
+		return RIGHT
+	case termbox.KeyArrowUp:
+		return UP
+	default:
+		return 0
+	}
+}
+
+func listenToKeyboard(evChan chan keyboardEvent) {
+	termbox.SetInputMode(termbox.InputEsc)
+
+	for {
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			switch ev.Key {
+			case termbox.KeyArrowLeft:
+				evChan <- keyboardEvent{eventType: MOVE, key: ev.Key}
+			case termbox.KeyArrowDown:
+				evChan <- keyboardEvent{eventType: MOVE, key: ev.Key}
+			case termbox.KeyArrowRight:
+				evChan <- keyboardEvent{eventType: MOVE, key: ev.Key}
+			case termbox.KeyArrowUp:
+				evChan <- keyboardEvent{eventType: MOVE, key: ev.Key}
+			case termbox.KeyEsc:
+				evChan <- keyboardEvent{eventType: END, key: ev.Key}
+			default:
+				if ev.Ch == 'r' {
+					evChan <- keyboardEvent{eventType: RETRY, key: ev.Key}
+				}
+			}
+		case termbox.EventError:
+			panic(ev.Err)
+		}
+	}
+}
